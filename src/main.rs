cd868dd952ef0608f6228a6c3b5594950fe62184
huangchao <huangchao@hotmail.com>
2017-09-22 19:44:41 +0800

    html response added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::fs::File;
 
 // curl -v 127.0.0.1:8080
 // as client, which will display the response message
@@ -18,13 +19,18 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
 	let mut buffer = [0; 512];
-
 	stream.read(&mut buffer).unwrap();
 
-	println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+	let mut file = File::open("hello.html").unwrap();
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).unwrap();
+
+	println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+	let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+	stream.write(response.as_bytes()).unwrap();
+	stream.flush().unwrap();
 
 }
