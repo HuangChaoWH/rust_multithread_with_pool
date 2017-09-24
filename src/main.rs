da007a5565a7eebc90cf43069d0023ab234a0092
huangchao <huangchao@hotmail.com>
2017-09-24 16:41:54 +0800

    :404 support and slow request simulation added

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
 
+use std::thread;
+use std::time::Duration;
+
 // curl -v 127.0.0.1:8080
 // as client, which will display the response message
 
 fn main() {
+	println!("\nTest in another terminal with command :\n\n curl -v http://localhost:8080\n\n");
+
 	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
 	for stream in listener.incoming() {
@@ -18,17 +23,34 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+
 	let mut buffer = [0; 512];
 	stream.read(&mut buffer).unwrap();
 
-	let mut file = File::open("hello.html").unwrap();
+	// simulate sleep
+	let   get = b"GET / HTTP/1.1\r\n";
+	let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+	let (status_line, filename) = if buffer.starts_with(get) {
+		("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+
+	} else if buffer.starts_with(sleep) {
+		thread::sleep(Duration::from_secs(2));
+		("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+
+	} else {
+		("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+	};
+
+	// ----
+	let mut file = File::open(filename).unwrap();
 
 	let mut contents = String::new();
 	file.read_to_string(&mut contents).unwrap();
 
 	println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-	let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+	let response = format!("{}\r\n\r\n{}", status_line, contents);
 
 	stream.write(response.as_bytes()).unwrap();
 	stream.flush().unwrap();
